957f9894f268241fc7cb0cc6bf7ba02e5c854cfb
ronaldo <ronaldomtlp125@gmail.com>
2024-03-19 09:30:30 -0500

    Configuracion básica y agregar dependencia en el pom.xml

---
+++ target/generated-sources/annotations/com/todocode/administradordelogins/logica/UsuarioCliente_.java
@@ -0,0 +1,16 @@
+package com.todocode.administradordelogins.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-03-19T08:34:13", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(UsuarioCliente.class)
+public class UsuarioCliente_ { 
+
+    public static volatile SingularAttribute<UsuarioCliente, String> usuario;
+    public static volatile SingularAttribute<UsuarioCliente, String> contrasenia;
+    public static volatile SingularAttribute<UsuarioCliente, Integer> id;
+    public static volatile SingularAttribute<UsuarioCliente, String> rol;
+
+}
